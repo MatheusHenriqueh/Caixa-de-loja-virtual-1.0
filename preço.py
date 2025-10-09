3d6511610fb0fb97c84d428d67e152c5b071665a
Matheus Ruivo <matheushenriquerodriguesruivo4@gmail.com>
2025-10-08 23:37:38 -0300

    Add files via upload

---
+++ preço.py
@@ -0,0 +1,14 @@
+#Calculadora de preços (loja)
+#Autor: Matheus Ruivo
+print("Bem-vindo(a) a MaxShop! ")
+print('Produzido por Matheus Ruivo!')
+print('Insira seus produtos, fazemos a soma!')
+print('Carregando Sistema...')
+pro1 = float(input("Qual é o preço do produto??"))
+pro2 = float(input("Qual é o preço do segundo produto??"))
+print("Carrgando...")
+somando1 = pro1 + pro2
+print(f"O preço do produto um e dois somando fica {somando1:.2f}")
+pro3 = float(input("Qual é o preço do terceiro produto??"))
+somando2 = somando1 + pro3
+print(f'Somando os três fica {somando2:.2f}.')
